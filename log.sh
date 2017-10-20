8727d46471e659e902e792f4b2524f88792fbd06
JamesSmith1202 <jsmith@stuy.edu>
2017-10-20 19:58:39 -0400

    Add files via upload

--- log.sh
+++ log.sh
@@ -4,12 +4,10 @@ DATE=`date '+%Y-%m-%d %H:%M:%S'`
 ENTRY="$1 -- $DATE\n$2\n"
 
 if [ $# -eq 2 ]; then
-    if [ -e devlog.txt ]; then
-        echo -e ""$ENTRY"" >> devlog.txt
-    else
+    if [ ! -e devlog.txt ]; then
         touch devlog.txt
-        echo -e "$ENTRY" >> devlog.txt
-    fi    
+    fi
+    echo -e "$ENTRY" >> devlog.txt   
 else
     echo -e "Incorrect number of arguments supplied\nUsage: $ ./log.sh <firstL> <message>"  
 fi
